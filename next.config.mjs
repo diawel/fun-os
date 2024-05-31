791eab352f2b972455e31bb6336e529e2badee35
Diawel <38136957+diawel@users.noreply.github.com>
2024-05-31 13:52:46 +0900

    init: vanilla extract

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
-const withVanillaExtract = createVanillaExtractPlugin();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = withVanillaExtract(nextConfig);
