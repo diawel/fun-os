3f9dbd5a5bbb66679b07ac33c2e8918df4096f98
addtobasic <addtobasic@gmail.com>
2024-06-17 06:45:02 +0900

    fix: Header, Footerをfigmaのdevモードのコードに修正

--- src/frames/Website/components/Footer/index.css.ts
+++ src/frames/Website/components/Footer/index.css.ts
@@ -1,29 +1,27 @@
 import { style } from '@vanilla-extract/css'
 
 export const footer = style({
-  backgroundColor: "#E46BA5",
-  width: "100%",
-  color: "white",
-  height: "160px",
+  backgroundColor: '#E46BA5',
+  width: '100%',
+  fontWeight: '800',
+  color: '#FFF',
+  height: '160px',
 })
 
 export const footerContent = style({
-  padding: "38px 0 0 43px",
+  padding: '38px 0 0 43px',
 })
 
 export const universityName = style({
-  fontSize: "14px",
-  fontWeight: "bold",
+  fontSize: '14px',
 })
 
 export const courseName = style({
-  fontSize: "14px",
-  fontWeight: "bold",
+  fontSize: '14px',
 
-  padding: "0 0 16px 0",
+  padding: '0 0 16px 0',
 })
 
 export const universityAddress = style({
-  fontSize: "13px",
-  fontWeight: "bold",
+  fontSize: '13px',
 })
