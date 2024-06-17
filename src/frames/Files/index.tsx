4c9012857c12c368be1e92fb7a913e270141e7da
Diawel <38136957+diawel@users.noreply.github.com>
2024-06-17 21:11:30 +0900

    add: サイドバースタイル

--- src/frames/Files/index.tsx
+++ src/frames/Files/index.tsx
@@ -9,7 +9,7 @@ const Files: FrameContent = ({ params }) => {
   const currentDirectory = findDirectory(directoryTree, params)
   return (
     <div className={styles.frame}>
-      <div>
+      <div className={styles.sidebar}>
         {directoryTree.children.map((child) => (
           <div key={child.name}>
             <button onClick={() => transition([child.name])}>
