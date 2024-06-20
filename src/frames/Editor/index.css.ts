962ad01499c549d477b3db470ec41af04b95df11
Diawel <38136957+diawel@users.noreply.github.com>
2024-06-21 03:17:32 +0900

    add: 再生の枠組み

--- src/frames/Editor/index.css.ts
+++ src/frames/Editor/index.css.ts
@@ -1,15 +1,57 @@
 import { style } from '@vanilla-extract/css'
 
-export const video = style({
-  width: 720,
-  aspectRatio: '1',
-  marginBottom: -8,
+export const frame = style({
+  width: 1000,
+  height: 600,
+  display: 'grid',
+  gridTemplateColumns: '2fr 3fr',
+  gridTemplateRows: '2fr 1fr',
+  backgroundColor: '#575C66',
+  color: '#fff',
+  padding: 10,
+  gap: 10,
 })
 
 export const error = style({
-  width: 720,
-  aspectRatio: '1',
+  borderRadius: 15,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
+  backgroundColor: '#303540',
+})
+
+export const preview = style({
+  width: '100%',
+  height: '100%',
+  backgroundColor: '#000',
+})
+
+export const mediapool = style({
+  padding: 16,
+  borderRadius: 15,
+  backgroundColor: '#303540',
+})
+
+export const timeline = style({
+  overflow: 'auto',
+  borderRadius: 15,
+  backgroundColor: '#303540',
+  gridColumn: '1 / 3',
+})
+
+export const timelineInner = style({
+  minWidth: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'flex-end',
+})
+
+export const timelineLine = style({
+  height: 16,
+  width: '100%',
+})
+
+export const timelineMediaLine = style({
+  height: 54,
+  width: '100%',
 })
--- src/frames/Editor/index.tsx
+++ src/frames/Editor/index.tsx
@@ -5,17 +5,27 @@ import { fileList } from './file-list'
 
 const Editor: FrameContent = ({ params }) => {
   if (!(params.join('/') in fileList))
-    return <div className={styles.error}>再生中の動画なし</div>
-
-  if (params.join('/') in fileList)
     return (
+      <div className={styles.frame}>
+        <div className={styles.mediapool} />
+        <div className={styles.error}>編集中のファイルなし</div>
+        <div className={styles.timeline} />
+      </div>
+    )
+
+  return (
+    <div className={styles.frame}>
+      <div className={styles.mediapool} />
+
       <Video
         src={fileList[params.join('/') as keyof typeof fileList]}
         controls
-        className={styles.video}
         autoPlay
+        className={styles.preview}
       />
-    )
+      <div className={styles.timeline} />
+    </div>
+  )
 }
 
 export default Editor
