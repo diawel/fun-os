cd8364d6c1c1fb6b0c883ac47a80136987ea2e25
Diawel <38136957+diawel@users.noreply.github.com>
2024-06-21 13:04:51 +0900

    add: 簡易テスト

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const params = new URL(document.location.href).searchParams
+const test = params.get('test')
+if (test) {
+  window.localStorage.setItem('TEST', test)
+  location.replace(location.href.split('?')[0])
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
